56c045e19160a2b4b34f85ef233942294b0b90f0
Zack Keller <zkeller89@users.noreply.github.com>
2024-08-07 16:49:46 +0000

    update

---
+++ 01-intro/run_elastic_search.sh
@@ -0,0 +1,7 @@
+docker run -it \
+    --name elasticsearch \
+    -p 9200:9200 \
+    -p 9300:9300 \
+    -e "discovery.type=single-node" \
+    -e "xpack.security.enabled=false" \
+    docker.elastic.co/elasticsearch/elasticsearch:8.4.3
